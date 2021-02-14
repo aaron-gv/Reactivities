fb920ddb707495a3b6c8324a049a23b1f801f917
aaron <a@a.com>
2021-02-14 18:24:02 +0000

    end of Section 11

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import './app/layout/style.css';
+import 'react-datepicker/dist/react-datepicker.css';
 import 'react-calendar/dist/Calendar.css';
 import 'react-toastify/dist/ReactToastify.min.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
-import { BrowserRouter, Router } from 'react-router-dom';
+import {  Router } from 'react-router-dom';
 import {createBrowserHistory} from 'history';
 
 export const history = createBrowserHistory();
